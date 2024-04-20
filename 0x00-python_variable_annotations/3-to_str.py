b70b4e87bcec29a2d7341f8641fe5b25422880e9
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-04-20 13:04:29 +0000

    Initial Commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    A type-annotated function floor which takes a
+    float n as argument and returns the floor of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Return the string version of the float n"""
+    return str(n)
