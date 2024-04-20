18e5b65299dbb79e5ff5942d7ca4c8de9d8bd86d
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-04-20 14:58:29 +0000

    edited

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,28 +1,27 @@
-#!/bin/usr/env python3
+#!/usr/bin/env python3
+"""Given the parameters and the return values, add type
+annotations to the function
+
+Hint: look into TypeVar
+
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
 """
-    Given the parameters and the return values,
-    add type annotations to the function
-"""
-from typing import TypeVar, Dict, Union, Mapping, Any
-
-T = TypeVar("T")
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
-    """
-    Safely retrieve a value from a dictionary.
+import typing
 
-    Parameters:
-        dct (Dict[T, value]): The dictionary to retrieve the value from.
-        key (T): The key to look up in the dictionary.
-        default (value, optional): The default value to return if the
-        key is not found. Defaults to None.
 
-    Returns:
-        value: The value corresponding to the key in the dictionary,
-        or the default value if the key is not found.
-    """
+T = typing.TypeVar('T')
+
+
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
+                     typing.Union[T, None] = None) -> \
+        typing.Union[typing.Any, T]:
+    """annotations of the function"""
     if key in dct:
         return dct[key]
     else:
