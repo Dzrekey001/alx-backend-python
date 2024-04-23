e31d1fae10e20e511a9b664723461e58f89f59af
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-04-23 23:58:50 +0000

    edited

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Asyncio task"""
 import asyncio
-from typing import List
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """returns a list if awaited async function"""
     start_time = time.time()
     funcs = [async_comprehension() for _ in range(4)]
